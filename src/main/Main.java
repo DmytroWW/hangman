dfccea50634790754922c49dd9576bb1d8fadbb7
Dmytro Kushnir <veksbrightside@gmail.com>
2025-05-13 14:36:09 +0300

    feat: add getRandomWord method

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,34 +7,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
+import java.util.Random;
+import java.util.logging.*;
+import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        getWordsFromFile();
-    }
-
     // Змінні
+    public static String selectedWord = "";
+    public static int wordLength = 5;
+
+    public static List<String> words = new ArrayList<>();
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
 
     //  Основна функція яка запускає інші функції відправляє println, отримує скани.
+    public static void main(String[] args) {
+        getWordsFromFile();
+        getRandomWordFromList();
+    }
+
 
     //  Функція яка добуває слова із файлу і формує масив слів.
-    public static List<String> getWordsFromFile() {
+    public static void getWordsFromFile() {
         String pathToWords = "src/resources/words.txt";
-        List<String> words = new ArrayList<>();
 
-        try (BufferedReader br = Files.newBufferedReader(Paths.get(pathToWords));){
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(pathToWords))){
             words = br.lines().collect(Collectors.toList());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Помилка при читанні файлу: " + pathToWords);
+            logger.log(Level.SEVERE, "Помилка при читанні файлу: " + pathToWords, e);
         }
-        System.out.println(words.size());
-        return words;
+
     }
 
     //
-    public static String getRandomWordFromFile() {
-        return "hello world";
+    public static void getRandomWordFromList() {
+        if (words.isEmpty()) {
+            System.out.println("Words list is empty");
+            return;
+        }
+
+        Random random = new Random();
+        String resultWord = "";
+        int attempts = 0;
+        while (resultWord.length() < wordLength) {
+            int randomNumber = random.nextInt(words.size());
+            resultWord = words.get(randomNumber);
+            attempts++;
+            if (attempts > 1000) {
+                return;
+            }
+        }
+
+         selectedWord = resultWord.replaceAll("\\s", "");
+         System.out.println("Random word: " + selectedWord);
+
     }
     //  Функція яка отримує із вводу букву і перевіряє чи коректний ввід.
     //  Функція яка створює дві змінні : слово яке відгадуємо та слово яке повертаємо користувачу(заштриховане).
